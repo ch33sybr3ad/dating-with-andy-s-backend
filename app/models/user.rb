a2eddcb8a0ccd5c47f71552d8257ea6ed542d9a9
Jason <jzhao.690@gmail.com>
2015-06-30 12:22:44 -0700

    generated models and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+  has_many :user_interests
+  has_many :interests, through: :user_interests
 end
--- db/migrate/20150630191208_create_user_interests.rb
+++ db/migrate/20150630191208_create_user_interests.rb
@@ -1,6 +1,8 @@
 class CreateUserInterests < ActiveRecord::Migration
   def change
     create_table :user_interests do |t|
+      t.belongs_to :user
+      t.belongs_to :interest
 
       t.timestamps null: false
     end
