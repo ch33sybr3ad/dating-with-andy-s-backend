0e57548f5e73931fd9b27fb1ce256944342eff53
Darwin D Wu <wuddarwin@gmail.com>
2015-06-30 14:54:58 -0700

    Add uniqueness validation for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email, :password, :age
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_length_of :password, minimum: 6
+  validates_uniqueness_of :username, :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    before(:all) { User.delete_all }
     describe User do
+        let(:keith) { User.create!(username: "keith", email: "keith@example.com", password: "123456", age: 27, bio: "I LOVE ANDY'S BACKEND!") }
 
         context "valid" do
             it "should successfully create a user" do
@@ -50,6 +52,24 @@ RSpec.describe User, type: :model do
                     email: "andy@example.com",
                     password: "123456",
                     bio: "I LOVE MY BACKEND!"
+                ) }.to raise_error(ActiveRecord::RecordInvalid)
+            end
+
+            it "should not create a user with a duplicate username" do
+                 expect{ User.create!(
+                    username: "keith",
+                    email: "abcd@example.com",
+                    password: "123456",
+                    bio: "I LOVE ANDY'S BACKEND!"
+                ) }.to raise_error(ActiveRecord::RecordInvalid)
+            end
+
+            it "should not create a user with a duplicate email" do
+                 expect{ User.create!(
+                    username: "keith2",
+                    email: "keith@example.com",
+                    password: "123456",
+                    bio: "I LOVE ANDY'S BACKEND!"
                 ) }.to raise_error(ActiveRecord::RecordInvalid)
             end
 
