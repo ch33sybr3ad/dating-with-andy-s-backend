5c636a44175d2c179e61eaec01951b260fbd8389
Jason <jzhao.690@gmail.com>
2015-06-30 18:25:17 -0700

    removed age validation, add to seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :user_interests
   has_many :interests, through: :user_interests
 
-  validates_presence_of :username, :email, :password, :age
+  validates_presence_of :username, :email, :password
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_length_of :password, minimum: 6
   validates_uniqueness_of :username, :email
